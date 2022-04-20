12326f85af02de3b8436c7c80ec8c6400ae84230
juanysofia1 <aguiarsofia77@gmail.com>
2022-04-20 10:21:57 -0500

    comparaciones

---
+++ comparaciones.js
@@ -0,0 +1,36 @@
+x=5;
+console.log(x>7);//false
+console.log(x<7);//true
+console.log(x=="5");//true
+console.log(x==="5");//false
+console.log(x>=1);//true
+console.log(x!=10);//true
+
+
+//2 punto 
+
+console.log(2 < 12 );//true 
+console.log(2 < "12"); //true
+console.log(2 < "John"); // false
+console.log(2 > "John"); //false
+console.log(2 == "John");//false 
+console.log("John" == "John"); //true
+console.log("john" == "John"); //false 
+console.log("2" < "12");	//false
+console.log("2" > "12");	//true
+console.log("2" == "12"); //false
+ 
+
+
+console.log("ejercicios")
+x=12;   
+y=7;
+x<15 && y!=7//false
+y>8 && x<20//false
+x==12 && x<18//true
+("lunes" == "Lunes") || (y!=9)//true
+!(x==10) && y>=6//true
+x>6 && y==12 && 2>3//false
+(x*2)>22 && y<(2*4)//true
+(x>10 || x<10) && !(y!=7 && y<21)//true
+((x/2)<=(2*3) || (x*2)>23 ) && (15==="15" && y!=="7")//false
